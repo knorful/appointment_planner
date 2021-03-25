5540d09ca9d117d68d5a4c69ec549e1120b4f483
Kris Norful <krisnorful@gmail.com>
2021-03-25 15:21:47 -0500

    add form to ContactsPage

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const ContactsPage = () => {
+export const ContactsPage = ({ contacts, addContact }) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,7 +25,13 @@ export const ContactsPage = () => {
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" id="name" name="name" /> <br></br>
+          <input type="email" id="email" name="email" /> <br></br>
+          <input type="number" id="phone" name="phone" /> <br></br>
+          <button>Add Contact</button>
+        </form>
       </section>
       <hr />
       <section>
