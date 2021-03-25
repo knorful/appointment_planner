86736e84cd4265ebf462bb610f28d08ad2d4c73a
Kris Norful <krisnorful@gmail.com>
2021-03-25 15:20:55 -0500

    remove unecessary tests from ContactPage that tested props passed in

--- src/__test__/ContactsPage.test.js
+++ src/__test__/ContactsPage.test.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import { ContactsPage } from "../containers/contactsPage/ContactsPage";
 
-const wrapper = shallow(<ContactsPage />);
-
 describe("<ContactsPage/> Component", () => {
+  // let wrapper;
+  // let props = {
+  //   contacts: { name: "Kris", number: "5016580226", email: "kris@email.com" },
+  //   addContact: jest.fn(),
+  // };
+
+  // beforeEach(() => {
+  //   wrapper = mount(<ContactsPage {...props} />);
+  // });
+
   it("renders correctly", () => {
     shallow(<ContactsPage />);
   });
