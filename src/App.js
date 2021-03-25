47110b15ff332794f40268d17bf92cbd1f1b7a1f
Kris Norful <krisnorful@gmail.com>
2021-03-25 09:24:45 -0500

    pass down contacts and appointments props/handlers to child components

--- src/App.js
+++ src/App.js
@@ -60,11 +60,15 @@ function App() {
           </Route>
           <Route path={ROUTES.CONTACTS}>
             {/* Add props to ContactsPage */}
-            <ContactsPage addContact={addContact} />
+            <ContactsPage contacts={contacts} addContact={addContact} />
           </Route>
           <Route path={ROUTES.APPOINTMENTS}>
             {/* Add props to AppointmentsPage */}
-            <AppointmentsPage />
+            <AppointmentsPage
+              appointments={appointments}
+              contacts={contacts}
+              addAppointment={addAppointment}
+            />
           </Route>
         </Switch>
       </main>
