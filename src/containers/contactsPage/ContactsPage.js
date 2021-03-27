aec7e168979d6348f1c7cce7eb7b7777c00f1055
Kris Norful <krisnorful@gmail.com>
2021-03-27 14:42:35 -0500

    check for duplicate names before state change

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -8,6 +8,7 @@ export const ContactsPage = ({ contacts, addContact }) => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,22 +16,48 @@ export const ContactsPage = ({ contacts, addContact }) => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    let name = e.target.name.value;
+    let email = e.target.email.value;
+    let phone = e.target.phone.value;
+
+    let hasError = contacts.find(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (!hasError) {
+      setName(name);
+      setPhone(phone);
+      setEmail(email);
+    }
   };
 
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
-
+  console.log("name: ", name);
   return (
     <div>
       <section>
         <h2>Add Contact</h2>
         <form onSubmit={handleSubmit}>
-          <input type="text" id="name" name="name" /> <br></br>
-          <input type="email" id="email" name="email" /> <br></br>
-          <input type="number" id="phone" name="phone" /> <br></br>
-          <button>Add Contact</button>
+          <input type="text" id="name" name="name" placeholder="Enter Name" />
+          <br></br>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter Email"
+          />
+          <br></br>
+          <input
+            type="number"
+            id="phone"
+            name="phone"
+            placeholder="Enter Phone"
+          />
+          <br></br>
+          <button id="submit-form">Add Contact</button>
         </form>
       </section>
       <hr />
