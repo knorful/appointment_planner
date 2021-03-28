befe2dfd05f5113c2ee6724958ed39e9457b48dd
Kris Norful <krisnorful@gmail.com>
2021-03-28 11:50:33 -0500

    give different class names to title and content of contact

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,5 +1,20 @@
 import React from "react";
 
 export const Tile = ({ contact }) => {
-  return <div className="tile-container"></div>;
+  let pSeperator = Object.values(contact).map((el, i) => {
+    if (i === 0) {
+      return (
+        <p key={i} className="tile-title">
+          {el}
+        </p>
+      );
+    } else {
+      return (
+        <p key={i} className="tile">
+          {el}
+        </p>
+      );
+    }
+  });
+  return <div className="tile-container">{pSeperator}</div>;
 };
