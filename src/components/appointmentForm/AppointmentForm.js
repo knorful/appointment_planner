778c97fa05fa1530486b0223764a1268d07bc20c
Kris Norful <krisnorful@gmail.com>
2021-03-28 12:54:03 -0500

    strigify date once selected

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
 export const AppointmentForm = ({
   contacts,
@@ -10,7 +11,7 @@ export const AppointmentForm = ({
   setDate,
   time,
   setTime,
-  handleSubmit
+  handleSubmit,
 }) => {
   const getTodayString = () => {
     const [month, day, year] = new Date()
@@ -19,7 +20,55 @@ export const AppointmentForm = ({
     return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
   };
 
+  const handleTitleChange = (e) => {
+    let titleChange = e.target.value;
+    setTitle(titleChange);
+  };
+
+  const handleDateChange = (e) => {
+    let dateChange = e.target.value;
+    setDate(dateChange);
+  };
+
+  const handleTimeChange = (e) => {
+    let timeChange = e.target.value;
+    setTime(timeChange);
+  };
+
+  const handleContactChange = (e) => {
+    let contactChange = e.target.value;
+    setContact(contactChange);
+  };
+
   return (
-    AppointmentForm
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="tile"
+        id="title"
+        value={title}
+        onChange={handleTitleChange}
+      />
+      <input
+        type="date"
+        name="date"
+        id="date"
+        value={date}
+        onChange={handleDateChange}
+        min={getTodayString()}
+      />
+      <input
+        type="time"
+        name="time"
+        id="time"
+        value={time}
+        onChange={handleTimeChange}
+      />
+      <ContactPicker
+        contacts={contacts}
+        handleContactChange={handleContactChange}
+      />
+      <input type="submit" value="Add Appointment" />
+    </form>
   );
 };
