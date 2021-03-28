ff22312b846335cfc52394de38f49123c0eb9411
Kris Norful <krisnorful@gmail.com>
2021-03-28 12:54:51 -0500

    create drop down to allow user to pick contact from list

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ contacts, handleContactChange }) => {
   return (
-    ContactPicker
+    <select name="contacts" id="contacts" onChange={handleContactChange}>
+      <option value="" selected="selected" disabled hidden>
+        Choose Contact
+      </option>
+      {contacts.map((contact, i) => (
+        <option key={i}>{Object.values(contact)[0]}</option>
+      ))}
+    </select>
   );
 };
