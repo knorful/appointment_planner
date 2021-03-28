188535511d360c23c8c790c45fac25502ca970fe
Kris Norful <krisnorful@gmail.com>
2021-03-28 11:23:34 -0500

    add form element to all users to add contacts

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -7,9 +7,39 @@ export const ContactForm = ({
   setPhone,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit,
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input
+        // onChange={handleNameChange}
+        type="text"
+        id="name"
+        name="name"
+        placeholder="Enter Name"
+        value={name}
+      />
+      <br></br>
+      <input
+        // onChange={handleEmailChange}
+        value={email}
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Enter Email"
+      />
+      <br></br>
+      <input
+        // onChange={handlePhoneChange}
+        value={phone}
+        pattern="^[2-9]\d{2}-\d{3}-\d{4}$"
+        type="tel"
+        id="phone"
+        name="phone"
+        placeholder="Enter Phone"
+      />
+      <br></br>
+      <button id="submit-form">Add Contact</button>
+    </form>
   );
 };
